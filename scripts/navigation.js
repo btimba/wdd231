971ff06476c18c587791999d8509fb1e63d49efc
Brian Timba <bryantymbr@gmail.com>
2026-01-12 09:48:42 +0200

    Refactor meta tags in index.html for improved SEO and update navigation.js for cleaner event handling; enhance base.css for better layout control

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,12 +1,10 @@
 const navButton = document.getElementById('nav-button');
-
-navButton.addEventListener('click', () => {
-navButton.classList.toggle('show');
-});
-
 const navBar = document.getElementById('nav-bar');
 
 navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-    navBar.classList.toggle('show');
+navButton.classList.toggle('show');
+navBar.classList.toggle('show');
 });
+
+
+
